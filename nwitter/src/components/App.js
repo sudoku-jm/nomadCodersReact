8500e7bdf67889e2f6a5371903992bc6685f7617
sudoku-jm <sudoku0630@gamail.com>
2022-06-19 18:58:07 +0900

    20220619 파이어베이스 이메일 로그인 기능

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
-import AppRouter from 'components/Router';
+import React, { useEffect, useState } from "react";
+import AppRouter from "components/Router";
 import { authService } from "fBase";
 
 const App = () => {
   // console.log(authService.currentUser)
+  const [init, setInit] = useState(false);
   //기본값 false
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //유저가 로그인 되어있는지 여부를 판단. 여기서는 실제 로그인 되었는지 로그아웃 한건지 잘 모름. 로그아웃했다고 알 수 도 있음. 그 이유는 firebase가 아직 시작되지도 않았기 때문이다.
+  // console.log("first", authService.currentUser); //null
+
+  // setInterval(() => {
+  //   console.log("check!!", authService.currentUser); //유저정보 뜸
+  // }, 2000);
+
+  useEffect(() => {
+    //페이지 진입 시 onAuthStateChanged함수를 통해 유저가
+    //실제 로그인이 되었는지 안 되었는지 알 수 있다.
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+
+      setInit(true);
+    });
+  }, []);
+
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn}/>
+      {/* init이 false가 되면 router를 숨길 것이다. */}
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+
       <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
     </>
-  )
+  );
 };
 
 export default App;
--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,3 +1,95 @@
-import React from 'react';
+import { async } from "@firebase/util";
+import { authService } from "fBase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import React, { useState } from "react";
 
-export default () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+
+  const onChange = (event) => {
+    // console.log(event.target.name)
+
+    const { name, value } = event.target;
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    //자동로그인
+    try {
+      if (newAccount) {
+        //새 계정 만들기
+        const data = await createUserWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+
+        console.log("data 새 계정 만들기", data);
+      } else {
+        //로그인
+        const data = await signInWithEmailAndPassword(
+          authService,
+          email,
+          password
+        );
+        console.log("data 로그인", data);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+
+  return (
+    <>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={email}
+            required
+            onChange={onChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={password}
+            onChange={onChange}
+            required
+          />
+          <input
+            type="submit"
+            value={newAccount ? "Create Account" : "Login"}
+          />
+          {error}
+        </form>
+        <span onClick={toggleAccount}>
+          {newAccount ? "Sign In" : "Create Account"}
+        </span>
+        <div>
+          <button>Continue with google</button>
+          <button>Continue with Github</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Auth;
